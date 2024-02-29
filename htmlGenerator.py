de2752d48b8ba32c7a7f16810913685974c8735f
mccartheney <mccartheney@hotmail.com>
2024-02-29 11:22:52 +0000

    method generateHtmlContent created

---
+++ htmlGenerator.py
@@ -0,0 +1,22 @@
+class htmlGenerator :
+    def __init__ (self) :
+        pass
+
+    def generateHtmlContent (self) :
+        #create html content
+        htmlContent = f'''
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <title>Document</title>
+        </head>
+        <body>
+            
+        </body>
+        </html>
+        '''
+
+        #return html content
+        return htmlContent
